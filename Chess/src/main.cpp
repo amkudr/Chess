701106657c8ec445b852697364f71ffdb0e57021
Anton Kudryashov <amkudr@mail.ru>
2024-06-11 17:44:41 +0700

    Check, king, rock is readu and work

--- Chess/src/main.cpp
+++ Chess/src/main.cpp
@@ -1,40 +1,54 @@
 // Chess 
 #include "Chess.h"
+#include "Engine.h"
 
 int main()
 {
-	string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr"; 
-//	string board = "##########K###############################R#############r#r#####";
-	Chess a(board);
+
+//	string board = "R##rKBNR###############################################prnbqkbnr";
+//	string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr";
+	string board = "##########K#############################################r#r#####";
+    Engine e(board);
+    cout<<e.printBoard();
+
+
+//    std::cout<<e.printBoard();
+
+    Chess a(board);
+
 	int codeResponse = 0;
+//    string res = "exit";
 	string res = a.getInput();
 	while (res != "exit")
 	{
-		/* 
-		codeResponse value : 
-		Illegal movements : 
-		11 - there is not piece at the source  
+		/*
+		codeResponse value :
+		Illegal movements :
+		11 - there is not piece at the source
 		12 - the piece in the source is piece of your opponent
-		13 - there one of your pieces at the destination 
-		21 - illegal movement of that piece 
+		13 - there one of your pieces at the destination
+		21 - illegal movement of that piece
 		31 - this movement will cause you checkmate
 
-		legal movements : 
-		41 - the last movement was legal and cause check 
-		42 - the last movement was legal, next turn 
+		legal movements :
+		41 - the last movement was legal and cause check
+		42 - the last movement was legal, next turn
 		*/
 
-		/**/ 
+		/**/
 		{ // put your code here instead that code
 			cout << "code response >> ";
-			cin >> codeResponse;
+
+			codeResponse = e.checkMove(res);
 		}
 		/**/
 
 		a.setCodeResponse(codeResponse);
-		res = a.getInput(); 
+        cout<<"Code response "<<codeResponse<<endl;
+		res = a.getInput();
 	}
 
-	cout << endl << "Exiting " << endl; 
+	cout << endl << "Exiting " << endl;
 	return 0;
 }
+
