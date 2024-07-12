a0f3e6b453da0ab4fd7845feb36c9f541abd5ea7
Anton Kudryashov <amkudr@mail.ru>
2024-07-12 14:08:39 +0700

    first move logic updated

--- Chess/include/Engine.h
+++ Chess/include/Engine.h
@@ -9,13 +9,31 @@ using namespace std;
 const int SIZE_B = 8;
 
 class Engine {
+    struct BoardState {
+        shared_ptr<Piece> board[SIZE_B][SIZE_B];
+        bool firstMove[SIZE_B][SIZE_B]{};
+
+        BoardState(shared_ptr<Piece> m_board[SIZE_B][SIZE_B], bool m_firstMove[SIZE_B][SIZE_B]) {
+            for (int i = 0; i < SIZE_B; i++) {
+                for (int j = 0; j < SIZE_B; j++) {
+                    board[i][j] = m_board[i][j];
+                    firstMove[i][j] = m_firstMove[i][j];
+                }
+            }
+        }
+    };
     shared_ptr<Piece> m_board[SIZE_B][SIZE_B] = {nullptr};
+    bool m_firstMove[SIZE_B][SIZE_B] = {false};
+
+    vector<BoardState> move_history;
+
     bool white_turn = true;
     shared_ptr<King> white_king;
     shared_ptr<King> black_king;
     bool isCheck(bool goodCheck);
     bool isCheckmate();
     bool Castling(int curX, int curY, int nextX, int nextY, const shared_ptr<Piece>& piece);
+    void undoMove();
 
 
 
@@ -25,7 +43,7 @@ public:
     int checkMove(string move, bool checkmateV);
     Engine &operator=(const Engine &) = delete;
     string printBoard();
-    void movePiece(int x_from, int y_from, int x_to, int y_to);
+    void movePiece(int x_from, int y_from, int x_to, int y_to, bool castlingRook);
 
 
 //    ~Engine();
--- Chess/src/Engine.cpp
+++ Chess/src/Engine.cpp
@@ -11,55 +11,46 @@ Engine::Engine(const string &start) {
 
     for (int col = 0; col < SIZE_B; col++) {
         for (int row = 0; row < SIZE_B; row++) {
-            switch (start[index]) {
-                case 'R':
-                    m_board[col][row] = make_shared<Rook>(col, row, true);
-                    break;
+            char pieceType = start[index++];
+            bool isWhite = isupper(pieceType);
+            pieceType = (char) tolower(pieceType);
+
+            shared_ptr<Piece> piece;
+            switch (pieceType) {
                 case 'r':
-                    m_board[col][row] = make_shared<Rook>(col, row, false);
-                    break;
-                case '#':
-                    m_board[col][row] = nullptr;
-                    break;
-                case 'K':
-                    m_board[col][row] = make_shared<King>(col, row, true);
-                    white_king = dynamic_pointer_cast<King>(m_board[col][row]);
+                    piece = make_shared<Rook>(col, row, isWhite);
                     break;
                 case 'k':
-                    m_board[col][row] = make_shared<King>(col, row, false);
-                    black_king = dynamic_pointer_cast<King>(m_board[col][row]);
-                    break;
-                case 'Q':
-                    m_board[col][row] = make_shared<Queen>(col, row, true);
+                    piece = make_shared<King>(col, row, isWhite);
                     break;
                 case 'q':
-                    m_board[col][row] = make_shared<Queen>(col, row, false);
-                    break;
-                case 'B':
-                    m_board[col][row] = make_shared<Bishop>(col, row, true);
+                    piece = make_shared<Queen>(col, row, isWhite);
                     break;
                 case 'b':
-                    m_board[col][row] = make_shared<Bishop>(col, row, false);
-                    break;
-                case 'P':
-                    m_board[col][row] = make_shared<Pawn>(col, row, true);
+                    piece = make_shared<Bishop>(col, row, isWhite);
                     break;
                 case 'p':
-                    m_board[col][row] = make_shared<Pawn>(col, row, false);
-                    break;
-                case 'N':
-                    m_board[col][row] = make_shared<Knight>(col, row, true);
+                    piece = make_shared<Pawn>(col, row, isWhite);
                     break;
                 case 'n':
-                    m_board[col][row] = make_shared<Knight>(col, row, false);
+                    piece = make_shared<Knight>(col, row, isWhite);
                     break;
-
+                case '#':
+                    piece = nullptr;
+                    break;
                 default:
-                    m_board[col][row] = make_shared<Piece>(col, row, true); //TODO later
+                    piece = make_shared<Piece>(col, row, isWhite); // Placeholder for future piece types
             }
-            index++;
+            m_board[col][row] = piece;
+            m_firstMove[col][row] = true;
+
+            if (pieceType == 'k') {
+                if (isWhite) white_king = dynamic_pointer_cast<King>(piece);
+                else black_king = dynamic_pointer_cast<King>(piece);
+            }
         }
     }
+
 
 }
 
@@ -123,9 +114,9 @@ int Engine::checkMove(string move, bool checkmateV) {
         }
     }
     bool isCastling = Castling(curX, curY, nextX, nextY, piece);
-    if(!isCastling) {
+    if (!isCastling) {
         if ((piece->getSymbol() == 'k' | piece->getSymbol() == 'K') && ((abs(nextY - curY) > 1) ||
-            abs(nextX - curX) > 1)) {
+                                                                        abs(nextX - curX) > 1)) {
             return 21; //Castling is not possible
         } else {
             vector<pair<int, int>> way = piece->getPotentialRoadblocks(nextX, nextY);
@@ -135,45 +126,28 @@ int Engine::checkMove(string move, bool checkmateV) {
                 }
             }
             // Make the move
-            movePiece(curX, curY, nextX, nextY);
+            movePiece(curX, curY, nextX, nextY, false);
         }
     }
 
     if (isCheck(false)) {
-        // if check, undo the move and castling if applicable
-        movePiece(nextX, nextY, curX, curY);
-        piece->setIsMoved(false);
-        if (isCastling) {
-            if (nextY == 6 && nextX == 0) {
-                movePiece(0, 5, 0, 7); // Undo white short castling
-                m_board[0][7]->setIsMoved(false);
-            } else if (nextY == 2 && nextX == 0) {
-                movePiece(0, 3, 0, 0); // Undo white long castling
-                m_board[0][0]->setIsMoved(false);
-            } else if (nextY == 6 && nextX == 7) {
-                m_board[7][7]->setIsMoved(false);
-                movePiece(7, 5, 7, 7); // Undo black short castling
-            } else if (nextY == 2 && nextX == 7) {
-                m_board[7][0]->setIsMoved(false);
-                movePiece(7, 3, 7, 0); // Undo black long castling
-            }
-        }
+        // if check, undo the move
+        undoMove();
         return 31;
     }
 
     // Check if the move causes check
     bool causeCheck = isCheck(true);
     if (causeCheck) {
-        if (!checkmateV && isCheckmate()) { // Check if the move causes checkmate only if checkmateV is true(not called from isCheckmate)
+        if (!checkmateV &&
+            isCheckmate()) { // Check if the move causes checkmate only if checkmateV is true(not called from isCheckmate)
             return 44; // checkmateV
         } else {
-            piece->setIsMoved(true);
-            return isCastling? 43:41; // Check
+            return isCastling ? 43 : 41; // Check
         }
     }
 
     white_turn = !white_turn;
-    piece->setIsMoved(true);
 
     return isCastling ? 43 : 42; // Castling or regular move
 }
@@ -217,17 +191,23 @@ bool Engine::isCheck(bool goodCheck) {
     return false;
 }
 
-void Engine::movePiece(int x_from, int y_from, int x_to, int y_to) {
+void Engine::movePiece(int x_from, int y_from, int x_to, int y_to, bool castlingRook) {
     /**
-     * Move piece from x_from, y_from to x_to, y_to
+     * Move piece from x_from, y_from to x_to, y_to and update the firstMove array
+     *
      * @param x_from - x coordinate of the piece
      * @param y_from - y coordinate of the piece
      * @param x_to - x coordinate of the destination
      * @param y_to - y coordinate of the destination
      */
+    if (!castlingRook) { // If rook castling, don't add the move to the history
+        move_history.emplace_back(m_board, m_firstMove);
+    }
     shared_ptr<Piece> piece = m_board[x_from][y_from];
     m_board[x_to][y_to] = piece;
     m_board[x_from][y_from] = nullptr;
+    m_firstMove[x_to][y_to] = true;
+    m_firstMove[x_from][y_from] = false;
     piece->setX(x_to);
     piece->setY(y_to);
     piece->setIsMoved(true);
@@ -247,11 +227,9 @@ bool Engine::isCheckmate() {
                     for (int yj = 0; yj < SIZE_B; yj++) {
                         string moveCheck =
                                 string(1, xi + 'a') + to_string(yi + 1) + string(1, xj + 'a') + to_string(yj + 1);
-                        bool isFirstMove = m_board[xi][yi]->isMoved();
                         int res = checkMove(moveCheck, true);
                         if (res == 42 || res == 41 || res == 43) {
-                            movePiece(xj, yj, xi, yi); // Undo the move
-                            m_board[xi][yi]->setIsMoved(isFirstMove);
+                            undoMove();
                             white_turn = !white_turn;
                             return false;
                         }
@@ -264,7 +242,7 @@ bool Engine::isCheckmate() {
     return true;
 }
 
-bool Engine::Castling(int curX, int curY, int nextX, int nextY, const shared_ptr<Piece>& piece) {
+bool Engine::Castling(int curX, int curY, int nextX, int nextY, const shared_ptr<Piece> &piece) {
     /**
      * Check if the move is castling
      * @param curX - x coordinate of the piece
@@ -278,30 +256,30 @@ bool Engine::Castling(int curX, int curY, int nextX, int nextY, const shared_ptr
          ((nextY == 6 && nextX == 0 &&
            m_board[0][5] == nullptr && m_board[0][6] == nullptr &&
            m_board[0][7] != nullptr && m_board[0][7]->getSymbol() == 'R' &&
-           !m_board[0][7]->isMoved() && !piece->isMoved()) ||
+           !m_firstMove[0][7] && !m_firstMove[curX][curY]) ||
           (nextY == 1 && nextX == 0 &&
            m_board[0][1] == nullptr && m_board[0][2] == nullptr && m_board[0][3] == nullptr &&
            m_board[0][0] != nullptr && m_board[0][0]->getSymbol() == 'R' &&
-           !m_board[0][0]->isMoved() && !piece->isMoved()))) ||
+           !m_firstMove[0][0] && !m_firstMove[curX][curY]))) ||
         (piece->getSymbol() == 'k' && curY == 4 && curX == 7 &&
          ((nextY == 6 && nextX == 7 &&
            m_board[7][5] == nullptr && m_board[7][6] == nullptr &&
            m_board[7][7] != nullptr && m_board[7][7]->getSymbol() == 'r' &&
-           !m_board[7][7]->isMoved() && !piece->isMoved()) ||
+           !m_firstMove[7][7] && !m_firstMove[curX][curY]) ||
           (nextY == 1 && nextX == 7 &&
            m_board[7][1] == nullptr && m_board[7][2] == nullptr && m_board[7][3] == nullptr &&
            m_board[7][0] != nullptr && m_board[7][0]->getSymbol() == 'r' &&
-           !m_board[7][0]->isMoved() && !piece->isMoved())))) {
-        movePiece(curX, curY, nextX, nextY);
+           !m_firstMove[7][0] && !m_firstMove[curX][curY])))) {
+        movePiece(curX, curY, nextX, nextY, false);
 
         if (nextY == 6 && nextX == 0) {
-            movePiece(0, 7, 0, 5); // White short castling
+            movePiece(0, 7, 0, 5, true); // White short castling
         } else if (nextY == 1 && nextX == 0) {
-            movePiece(0, 0, 0, 3); // White long castling
+            movePiece(0, 0, 0, 3, true); // White long castling
         } else if (nextY == 6) {
-            movePiece(7, 7, 7, 5); // Black short castling
-        } else  {
-            movePiece(7, 0, 7, 3); // Black long castling
+            movePiece(7, 7, 7, 5, true); // Black short castling
+        } else {
+            movePiece(7, 0, 7, 3, true); // Black long castling
         }
         return true;
 
@@ -309,3 +287,24 @@ bool Engine::Castling(int curX, int curY, int nextX, int nextY, const shared_ptr
     return false;
 }
 
+void Engine::undoMove() {
+    /**
+     * Undo the last move
+     */
+    if (!move_history.empty()) {
+        BoardState lastMove = move_history.back();
+        for (int i = 0; i < SIZE_B; i++) {
+            for (int j = 0; j < SIZE_B; j++) {
+                m_board[i][j] = lastMove.board[i][j];
+                m_firstMove[i][j] = lastMove.firstMove[i][j];
+                if (m_board[i][j] != nullptr) {
+                    m_board[i][j]->setX(i);
+                    m_board[i][j]->setY(j);
+                    m_board[i][j]->setIsMoved(m_firstMove[i][j]);
+                }
+            }
+        }
+        move_history.pop_back();
+    }
+
+}
